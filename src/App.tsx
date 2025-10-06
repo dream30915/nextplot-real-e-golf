e7b2365127af6e904e8b67ed99672f0cc74004f5
Dump <dream30915@gmail.com>
2025-10-06 18:17:50 +0000

    Generated by Spark: Fix all reported errors.

--- src/App.tsx
+++ src/App.tsx
@@ -1452,8 +1452,8 @@ function App() {
         </div>
       </section>
 
-      {/* Properties Grid */}
-      <main id="main-content" className="py-8" role="main">
+      {/* Information Cards */}
+      <section className="py-8">
         <div className="container mx-auto px-4">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             {/* About NextPlot */}
@@ -1660,6 +1660,7 @@ function App() {
           </div>
         </div>
       </section>
+      
       {/* Properties Grid */}
       <main id="main-content" className="py-8" role="main">
         <div className="container mx-auto px-4">
