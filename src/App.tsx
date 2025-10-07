2b62c92edce1aa9650edb5a6110adfb926024deb
Dump <dream30915@gmail.com>
2025-10-07 14:46:45 +0000

    Generated by Spark: เอาลูกศรออกให้เหลือแต่ตัวอักษร

--- src/App.tsx
+++ src/App.tsx
@@ -1163,7 +1163,7 @@ function App() {
               
               {/* Language Selector */}
               <Select value={currentLang} onValueChange={changeLanguage}>
-                <SelectTrigger className="w-16">
+                <SelectTrigger className="w-16 [&>svg]:hidden">
                   <SelectValue />
                 </SelectTrigger>
                 <SelectContent>
